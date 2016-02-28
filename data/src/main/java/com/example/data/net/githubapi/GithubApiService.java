fa1fad4a0a9cd10248b6cfb8a448e68a65770db0
vichid <victor.hidalgo.lorenzo@gmail.com>
2016-02-28 10:30:01 +0100

    Bumped dependencies

--- data/src/main/java/com/example/data/net/githubapi/GithubApiService.java
+++ data/src/main/java/com/example/data/net/githubapi/GithubApiService.java
@@ -18,9 +18,9 @@ package com.example.data.net.githubapi;
 
 import com.example.data.entity.RepoEntity;
 import java.util.List;
-import retrofit.Call;
-import retrofit.http.GET;
-import retrofit.http.Path;
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
 
 public interface GithubApiService {
 
